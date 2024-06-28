089f505dccc576b6e9512c30489dc3ce0925f805
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-06-28 19:01:37 -0300

    test: ensure status code is 201

--- tests/test_subjects.py
+++ tests/test_subjects.py
@@ -1,11 +1,12 @@
+from http import HTTPStatus
 
 
 def test_create_subject(client):
-    response = client.post("/api/v1/subjects/", json={
-        'name': 'História'
-    })
+    response = client.post('/api/v1/subjects/', json={'name': 'História'})
 
     assert response.json() == {
         'id': 1,
         'name': 'História',
     }
+
+    assert response.status_code == HTTPStatus.CREATED
